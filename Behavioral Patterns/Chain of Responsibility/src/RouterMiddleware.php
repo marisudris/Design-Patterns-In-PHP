904fc5598aa81bf87332b92367afaf13ad4804a7
Maris Udris <maris.udris.lv@gmail.com>
2019-12-25 23:34:39 +0200

    Added missing method skeleton

--- Behavioral Patterns/Chain of Responsibility/src/RouterMiddleware.php
+++ Behavioral Patterns/Chain of Responsibility/src/RouterMiddleware.php
@@ -20,6 +20,11 @@ class RouterMiddleware extends AbstractMiddleware
         return $handler->handle($request);
     }
 
+    private function routeExists(ServerRequestInterface $request): bool
+    {
+        // check if route is set
+    }
+
     private function dispatch(ServerRequestInterface $request): ResponseInterface 
     {
         // call the proper handler (controller) and return a response
