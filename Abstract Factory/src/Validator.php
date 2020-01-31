b4ce92f2b1cc35db2e83fbbf4edc2d87c356255d
Maris Udris <maris.udris.lv@gmail.com>
2020-01-31 20:33:03 +0200

    Added Dockblocks for the classes

--- Abstract Factory/src/Validator.php
+++ Abstract Factory/src/Validator.php
@@ -3,6 +3,12 @@ declare(strict_types=1);
 
 namespace harlequiin\Patterns\AbstractFactory;
 
+/**
+ * Product.
+ *
+ * An interface for one of the Products in the product
+ * family.
+ */
 interface Validator
 {
     public function validate(string $str): bool;
